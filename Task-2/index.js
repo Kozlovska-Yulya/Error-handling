5bb07568c80b24222fa40f85d23f90aefc5eb28d
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-04-20 17:16:06 +0300

    promise error

--- Task-2/index.js
+++ Task-2/index.js
@@ -2,7 +2,6 @@ const successRequest = Promise.resolve({ name: 'Tom' });
 
 successRequest
   .then(function onSuccess1(data) {
-    console.log(data);
     throw new Error('Error with data');
   })
   .catch(function onError1(error) {
